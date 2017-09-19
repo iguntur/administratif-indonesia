e65245c0bc2166d77f73360bb1e4176d6e3e796d
Guntur Poetra <poetra.guntur@gmail.com>
2017-09-20 01:57:22 +0700

    make NYC happy

--- test/code.js
+++ test/code.js
@@ -1,6 +1,12 @@
 import test from 'ava';
 import c from '../lib/code';
 
+test('throws', t => {
+	t.throws(() => c.isValid(), TypeError);
+	t.throws(() => c.isValid([]), TypeError);
+	t.throws(() => c.isValid({}), TypeError);
+});
+
 test('code.isValid()', t => {
 	t.true(c.isValid('31'));
 	t.true(c.isValid('10.11'));
@@ -30,4 +36,10 @@ test('code.type()', t => {
 	t.is(c.type('31.01.10'), 'kecamatan');
 	t.is(c.type('31.01.10.1001'), 'kelurahan');
 	t.is(c.type('31.01.10.2001'), 'desa');
+
+	t.is(c.type('1'), null);
+	t.is(c.type('333'), null);
+	t.is(c.type('22.1'), null);
+	t.is(c.type('22.22.333'), null);
+	t.is(c.type('22.22.22.333'), null);
 });
