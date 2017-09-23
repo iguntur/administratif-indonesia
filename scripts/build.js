f69a870581fd08456b8f58ac0dc1a3bea8c006e9
Guntur Poetra <poetra.guntur@gmail.com>
2017-09-24 05:31:25 +0700

    minor tweaks

--- scripts/build.js
+++ scripts/build.js
@@ -1,17 +1,30 @@
 'use strict';
 const fs = require('fs');
 const del = require('del');
+const util = require('util');
 const paths = require('../lib/paths');
 const builder = require('../lib/builder');
 
-Promise.resolve()
-	.then(() => del([paths('dist')]))
-	.then(() => {
-		builder.run().then(results => results.forEach(res => {
-			const ws = fs.createWriteStream(paths('dist', res.filename));
-			ws.write(JSON.stringify(res.ctx, null, '\t'));
-			ws.end('\n');
-		}));
+const fsP = {
+	writeFile: util.promisify(fs.writeFile)
+};
+
+const saveAs = (filepath, data) => {
+	data = JSON.stringify(data, null, '\t');
+	return fsP.writeFile(filepath, data);
+};
+
+const buildDist = (stats) => {
+	stats.forEach(obj => {
+		saveAs(paths('dist', obj.filename), obj.ctx)
+	});
+};
+
+del([paths('dist')]);
+
+builder.run()
+	.then((stats) => {
+		buildDist(stats);
 	})
 	.catch(err => {
 		console.error(err);
